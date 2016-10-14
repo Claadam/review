379d4a1bcd2cf6dd63c37e7f7412964862c778fb
WalnutsAreCool <claadam@nuevaschool.org>
2016-10-14 09:14:01 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+console.log("loaded")
 var a = $("#factor one")
 var b = $("#factor two")
 var product = $("#product")
 var input = $(".input")
+console.log(a)
+console.log(b)
 
 function multiplication (arg1,arg2) {
+  console.log("called")
  var c = arg1*arg2
  product.html(c)
 }
 
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
+    console.log("enter")
     multiplication(a,b)
   }
 })
